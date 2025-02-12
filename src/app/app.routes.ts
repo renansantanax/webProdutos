ca491fb5e4b6d9a9b43f9e54da14bd44879bfcf6
nansant <renan.2r@live.com>
2025-02-11 21:17:50 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { CadastroProdutosComponent } from './components/pages/cadastro-produtos/cadastro-produtos.component';
+import { ConsultaProdutosComponent } from './components/pages/consulta-produtos/consulta-produtos.component';
+import { EdicaoProdutosComponent } from './components/pages/edicao-produtos/edicao-produtos.component';
 
-export const routes: Routes = [];
+// Configuração de rotas
+export const routes: Routes = [
+    {
+        path: 'pages/cadastro-produtos', // ROTA
+        component: CadastroProdutosComponent
+    },
+    {
+        path: 'pages/consulta-produtos', // ROTA
+        component: ConsultaProdutosComponent
+    },
+    {
+        path: 'pages/edicao-produtos', // ROTA
+        component: EdicaoProdutosComponent
+    }
+];
