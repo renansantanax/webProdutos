bddaa1ab3a22dbdc77fa41a5771f2ed9d73dd4cd
Matheus Peluso <matheuspeluso17@gmail.com>
2025-02-18 22:01:47 -0300

    inserido formulário de cadastro autenticando com a API do backend

--- src/app/components/pages/cadastro-produtos/cadastro-produtos.component.ts
+++ src/app/components/pages/cadastro-produtos/cadastro-produtos.component.ts
@@ -1,16 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-produtos',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './cadastro-produtos.component.html',
   styleUrl: './cadastro-produtos.component.css',
 })
 export class CadastroProdutosComponent {
   //atributos do componente
   categorias: any[] = []; //array de objetos
+
+  // funcao executada ao abrir a pagina
+  ngOnInit() { }
 
   //método construtor (inicializar objetos)
   constructor(private http: HttpClient) {
@@ -23,6 +31,29 @@ export class CadastroProdutosComponent {
     });
   }
 
-  // funcao executada ao abrir a pagina
-  ngOnInit() {}
+  //objeto para capturar os campos do formulário
+  form = new FormGroup({
+    nome: new FormControl(''), // campo 'nome'
+    preco: new FormControl(''), // campo 'preco'
+    quantidade: new FormControl(''), // campo 'quantidade'
+    categoriaId: new FormControl('') // campo 'categoriaId'
+  });
+
+  // função executada ao enviar o formulário
+  onSubmit() {
+
+    // fazendo a requisição POST para API
+    this.http.post('http://localhost:8080/api/produtos/cadastrar', this.form.value,
+      { responseType: 'text' }
+    ).subscribe({ //aguardando o retorno da API
+      next: (data) => { // se a req for bem sucedida
+        console.log(data);
+      },
+      error: (e) => { // se a requisição falhar
+        console.log(e.error);
+      }
+    });
+
+  }
+
 }
