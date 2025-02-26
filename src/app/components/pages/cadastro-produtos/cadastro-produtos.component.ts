db9aeeb3b9eb6c6da2077b51bb0c284d732d8805
renansantanax <renan.2r@live.com>
2025-02-25 21:23:14 -0300

    minor changes

--- src/app/components/pages/cadastro-produtos/cadastro-produtos.component.ts
+++ src/app/components/pages/cadastro-produtos/cadastro-produtos.component.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { endpoints } from '../../../configurations/environments';
 
 @Component({
   selector: 'app-cadastro-produtos',
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule
-  ],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './cadastro-produtos.component.html',
   styleUrl: './cadastro-produtos.component.css',
 })
@@ -20,16 +22,16 @@ export class CadastroProdutosComponent {
   mensagem: string = ''; // mensagem de sucesso
 
   // funcao executada ao abrir a pagina
-  ngOnInit() { }
+  ngOnInit() {}
 
   //método construtor (inicializar objetos)
   constructor(private http: HttpClient) {
     // fazendo uma requisição GET para a API
-    this.http.get('http://localhost:8080/api/categorias/consultar').subscribe({
+    this.http.get(`${endpoints.consultar_categorias}`).subscribe({
       // capturar a resposta da requisição
       next: (data) => {
         this.categorias = data as any[];
-      }
+      },
     });
   }
 
@@ -38,32 +40,34 @@ export class CadastroProdutosComponent {
     nome: new FormControl(''), // campo 'nome'
     preco: new FormControl(''), // campo 'preco'
     quantidade: new FormControl(''), // campo 'quantidade'
-    categoriaId: new FormControl('') // campo 'categoriaId'
+    categoriaId: new FormControl(''), // campo 'categoriaId'
   });
 
   // função executada ao enviar o formulário
   onSubmit() {
-
     // fazendo a requisição POST para API
-    this.http.post('http://localhost:8080/api/produtos/cadastrar', this.form.value,
-      { responseType: 'text' }
-    ).subscribe({ //aguardando o retorno da API
-      next: (data) => { // se a req for bem sucedida
-        //limpar o erros
-        this.erros = null;
+    this.http
+      .post(`${endpoints.cadastrar_produto}`, this.form.value, {
+        responseType: 'text',
+      })
+      .subscribe({
+        //aguardando o retorno da API
+        next: (data) => {
+          // se a req for bem sucedida
+          //limpar o erros
+          this.erros = null;
 
-        //capturar a msg de sucesso da api
-        this.mensagem = data;
+          //capturar a msg de sucesso da api
+          this.mensagem = data;
 
-        //zerar os formulário
-        this.form.reset();
-      },
-      error: (e) => { // se a requisição falhar
-        this.erros = JSON.parse(e.error);
-        this.mensagem = '';
-      }
-    });
-
+          //zerar os formulário
+          this.form.reset();
+        },
+        error: (e) => {
+          // se a requisição falhar
+          this.erros = JSON.parse(e.error);
+          this.mensagem = '';
+        },
+      });
   }
-
 }
--- src/app/components/pages/consulta-produtos/consulta-produtos.component.ts
+++ src/app/components/pages/consulta-produtos/consulta-produtos.component.ts
@@ -1,41 +1,55 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { endpoints } from '../../../configurations/environments';
 
 @Component({
   selector: 'app-consulta-produtos',
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule
-  ],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './consulta-produtos.component.html',
-  styleUrl: './consulta-produtos.component.css'
+  styleUrl: './consulta-produtos.component.css',
 })
 export class ConsultaProdutosComponent {
-
-  produtos: any[] = [];
+  produtos: any[] = []; //array de objetos
+  mensagem: string = ''; //mensagem de texto
 
   //método construtor
-  constructor(private http: HttpClient) { }
-
+  constructor(private http: HttpClient) {}
 
   //criando um formulário para capturar a pesquisa de produtos
   form = new FormGroup({
-    nome: new FormControl('') // campo 'nome'
+    nome: new FormControl(''), // campo 'nome'
   });
-
 
   //função para enviar os dados para a api
   onSubmit() {
-
-    this.http.get('http://localhost:8080/api/produtos/consultar/' + this.form.value.nome).subscribe({
-      next: (data) => {
-        this.produtos = data as any[];
-      }
-    })
-
+    this.http
+      .get(`${endpoints.consultar_produtos}/${this.form.value.nome}`)
+      .subscribe({
+        next: (data) => {
+          this.produtos = data as any[];
+        },
+      });
   }
 
+  // função para enviar uma request de exclusão de produto para a API
+  onDelete(id: string) {
+    //pedir ao usuário que confirme a operação
+    if (confirm('Deseja realmente excluir o produto selecionado?')) {
+      this.http
+        .delete(`${endpoints.excluir_produto}/${id}`, { responseType: 'text' })
+        .subscribe({
+          next: (data) => {
+            this.mensagem = data;
+            this.onSubmit();
+          },
+        });
+    }
+  }
 }
